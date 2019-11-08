cd165102d7d43e92e15b08b5e07c18e23f036ea0
Jean-Sebastien BEVILACQUA <realitix@gmail.com>
2019-11-08 17:12:41 +0100

    Fix bug

--- generate.py
+++ generate.py
@@ -3,9 +3,11 @@
 import yaml
 from yamlinclude import YamlIncludeConstructor
 from os import path
+import os
 import glob
 import json
 import re
+import io
 
 
 GLOBALS = {
@@ -30,18 +32,23 @@ YamlIncludeConstructor.add_to_loader_class(loader_class=yaml.FullLoader,
 
 
 src_files = []
+
 for src_file in glob.glob(path.join(SRC, "*.yml")):
     filename, _ = path.splitext(path.basename(src_file))
 
     # Read src ile
-    with open(src_file, 'r') as f:
+    with io.open(src_file, 'r', encoding='utf8') as f:
         content = f.read()
         
         data = yaml.load(content, Loader=yaml.FullLoader)
         json_str = json.dumps(data, indent=4)
         json_to_write = replace_in_content(json_str)
 
-        # Write out file
         out_file_path = path.join(OUT, f"{filename}.json")
-        with open(out_file_path, 'w') as outfile:
+
+        # Delete out file before writing it (i got a bug)
+        os.remove(out_file_path)
+
+        # Write out file
+        with io.open(out_file_path, 'w', encoding='utf8') as outfile:
             outfile.write(json_to_write)
